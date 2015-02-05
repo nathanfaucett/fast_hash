07f68a810d9738d86e47ce476e697be67436097a
Nathan Faucett <nathanfaucett@gmail.com>
2015-02-05 17:39:18 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+FastHash
+=======
+
+FastHash for the browser and node.js
---
+++ src/index.js
@@ -0,0 +1,69 @@
+var has = require("has"),
+    indexOf = require("index_of"),
+    forEach = require("for_each");
+
+
+module.exports = FastHash;
+
+
+function FastHash(key) {
+    this.__key = key;
+    this.__array = [];
+    this.__hash = {};
+}
+
+FastHash.prototype.get = function(key) {
+    return this.__hash[key];
+};
+
+FastHash.prototype.has = function(key) {
+    return has(this.__hash, key);
+};
+
+FastHash.prototype.add = function() {
+    var i = -1,
+        il = arguments.length - 1;
+
+    while (i++ < il) {
+        FastHash_add(this, arguments[i]);
+    }
+
+    return this;
+};
+
+function FastHash_add(_this, value) {
+    var array = _this.__array,
+        hash = _this.__hash,
+        key = value[_this.__key];
+
+    if (!has(hash, key)) {
+        hash[key] = value;
+        array[array.length] = value;
+    }
+}
+
+FastHash.prototype.remove = function() {
+    var i = -1,
+        il = arguments.length - 1;
+
+    while (i++ < il) {
+        FastHash_remove(this, arguments[i]);
+    }
+
+    return this;
+};
+
+function FastHash_remove(_this, value) {
+    var array = _this.__array,
+        hash = _this.__hash,
+        key = value[_this.__key];
+
+    if (has(hash, key)) {
+        delete hash[key];
+        array.splice(indexOf(value), 1);
+    }
+}
+
+FastHash.prototype.forEach = function(callback, thisArg) {
+    return forEach(this.__array, callback, thisArg);
+};
